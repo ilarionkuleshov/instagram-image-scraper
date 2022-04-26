4c394b7fb3518580813e08d2f59c0a89f594d8a2
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2022-04-26 19:38:43 +0300

    edit scrapy settings

--- imagescraper/imagescraper/settings.py
+++ imagescraper/imagescraper/settings.py
@@ -7,17 +7,26 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+from shutil import which
+
+
 BOT_NAME = 'imagescraper'
 
 SPIDER_MODULES = ['imagescraper.spiders']
 NEWSPIDER_MODULE = 'imagescraper.spiders'
 
+SELENIUM_DRIVER_NAME = 'chrome'
+SELENIUM_DRIVER_EXECUTABLE_PATH = which('./chromedriver')
+SELENIUM_DRIVER_ARGUMENTS=[] # '-headless'
+
+ITEM_PIPELINES = {'scrapy.pipelines.images.ImagesPipeline': 1}
+IMAGES_STORE = 'images'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'imagescraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -50,9 +59,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'imagescraper.middlewares.ImagescraperDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+	#'imagescraper.middlewares.ImagescraperDownloaderMiddleware': 543,
+	'scrapy_selenium.SeleniumMiddleware': 800,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
